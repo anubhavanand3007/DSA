9c46a5549faf75a60367843296588157a6b5b87b
Anubav Anand <anubhavanand3007@gmail.com>
2021-10-20 17:16:22 +0530

    ques 1

--- ques1.cpp
+++ ques1.cpp
@@ -1,10 +1,15 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    for(int i=0;i<5;i++){
-        cout << i << " ";
+class Solution {
+public:
+     int sumOfUnique(vector<int>& nums) {
+	 // freq array counts the number of times an element occurs in an array.
+        int freq[101] = {0};
+        int sum = 0,n = nums.size();
+        for(int i = 0; i < n;i++)  
+            freq[nums[i]]++;
+        for(int i =0;i < n;i++){
+            if(freq[nums[i]] == 1)
+                sum += nums[i];
+        }
+        return sum;
     }
-    cout << "Anubhav";
-    return 0;
-}
+};
